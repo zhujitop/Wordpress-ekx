8060d76224f428844c064a440d3ff7ca3849db93
zhujitop <35312148+zhujitop@users.noreply.github.com>
2022-12-12 20:35:57 +0800

    Update functionsekx.php

--- functionsekx.php
+++ functionsekx.php
@@ -1,7 +1,8 @@
 <?php
-// mouchajiankaixindaima
+// 会员下载
 
 $tx_count = $wpdb->get_var("SELECT count(ice_id) FROM $wpdb->iceget where ice_success != 1");
 		if (function_exists('add_menu_page')) {
 }
+//这一段删掉
 ?>
